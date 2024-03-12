ecdc087568678f703bba1694782ceb810738fa74
axsxpro <cmptpro@outlook.com>
2024-03-12 08:50:03 +0100

    ajout EasyAdmin

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Animation;
+use App\Entity\AnimationStudio;
+use App\Entity\Category;
+use App\Entity\Episode;
+use App\Entity\Type;
+use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractDashboardController
+{
+    #[Route('/admin', name: 'admin')]
+    public function index(): Response
+    {
+        
+        // $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
+        // return $this->redirect($adminUrlGenerator->setController(UserCrudController::class)->generateUrl());
+
+        return $this->render('admin/dashboard.html.twig');
+
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('DashBoard Admin');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linkToCrud('Users', 'fa fa-user-circle', User::class);
+        yield MenuItem::linkToCrud('Episodes', 'fa fa-television', Episode::class);
+        yield MenuItem::linkToCrud('Animations', 'fa fa-film', Animation::class);
+        yield MenuItem::linkToCrud('Animation Studios', 'fa fa-video-camera', AnimationStudio::class);
+        yield MenuItem::linkToCrud('Categories', 'fa fa-tags', Category::class);
+        yield MenuItem::linkToCrud('Types', 'fa fa-list-alt', Type::class);
+    }
+}
